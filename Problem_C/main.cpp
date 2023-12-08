24b6fe7c8c057df50c6cd8f64f1d35dd1485180d
Galaxy-Wizard <KSA811227@OUTLOOK.COM>
2023-12-08 07:30:34 +0300

    Часть слагаемых теряется.

--- Problem_C/main.cpp
+++ Problem_C/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 //#include <fstream>
 
@@ -122,7 +122,7 @@ unsigned long long s_bits(unsigned long long n, const unsigned long long modul)
 
 		set_bits_quantity_by_bit.resize(n_bits_size);
 
-		for (unsigned long long counter = 0; counter <= c; counter++)
+		for (unsigned long long counter = 1; counter <= c; counter++)
 		{
 			for (long n_bits_counter = 0; n_bits_counter < n_bits_size; n_bits_counter++)
 			{
@@ -140,9 +140,13 @@ unsigned long long s_bits(unsigned long long n, const unsigned long long modul)
 
 		unsigned long long result = 0;
 
-		for (long n_bits_counter = 0; n_bits_counter < n_bits_size; n_bits_counter++)
+		for (unsigned long long counter = 1; counter <= c; counter++)
 		{
-			//result += unsigned long long(set_bits_quantity_by_bit[n_bits_counter]) * (unsigned long long(1) << n_bits_counter);
+			for (long n_bits_counter = 0; n_bits_counter < n_bits_size; n_bits_counter++)
+			{
+				//	Здесь теряются слагаемые в которых несколько бит установлены.
+				result += counter & (unsigned long long(set_bits_quantity_by_bit[n_bits_counter]) * (unsigned long long(1) << n_bits_counter));
+			}
 		}
 
 		result %= modul;
